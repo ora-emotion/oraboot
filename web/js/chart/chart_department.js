65cf013ddd2311f32d6dc9ffc298c79c59fe20e7
smpower <bzsjxhywrf@outlook.com>
2018-01-30 13:13:02 +0800

    业绩图表模块完成部门总数据图表的渲染和分部数据图表的渲染;

--- web/js/chart/chart_department.js
+++ web/js/chart/chart_department.js
@@ -18,11 +18,10 @@
 var departmentChart = (function () {
   // ---------------------- BEGIN MODULE SCOPE VARIABLES ---------------------
   var
-    configMap = {},
     stateMap = {},
     jqueryMap = {},
 
-    setJqueryMap, getData, renderChart, initModule
+    setJqueryMap, getData, renderChart, renderBranchData, onClick, initModule
   ;
   // ----------------------- END MODULE SCOPE VARIABLES ----------------------
 
@@ -37,6 +36,11 @@ var departmentChart = (function () {
       type : 'post',
       url  : url,
       success : function (data) {
+        if (data.length === 0) { alert('暂无数据'); return false; }
+
+        $('.chart-wrap .chart').remove();
+        $('.chart-wrap').append('<div class="chart"></div>');
+
         // data [[], [], []] - 计划 实际 完成率
         if (fn_map.hasOwnProperty('suc')) {
           fn_map.suc(data);
@@ -65,7 +69,11 @@ var departmentChart = (function () {
 
   // Begin DOM method /renderChart/
   renderChart = function (data) {
-    var myChart, option;
+    var i, myChart, option, month_arr = [];
+
+    for (i = 0; i < data[0].length; i++) {
+      month_arr.push(i + 1 + '月');
+    }
 
     myChart = echarts.init(document.getElementsByClassName('chart')[0]);
 
@@ -93,7 +101,7 @@ var departmentChart = (function () {
       xAxis: [
         {
           type: 'category',
-          data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+          data: month_arr,
           axisPointer: {
             type: 'shadow'
           }
@@ -144,10 +152,142 @@ var departmentChart = (function () {
     myChart.setOption(option);
   };
   // End DOM method /renderChart/
+
+  // Begin DOM method /renderBranchData/
+  renderBranchData = function (data) {
+    var i, myChart, option, month_arr = [];
+
+    for (i = 0; i < data[0][0].length; i++) {
+      // 获取总月份
+      month_arr.push(i + 1 + '月');
+    }
+
+    myChart = echarts.init(document.getElementsByClassName('chart')[0]);
+
+    option = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          crossStyle: {
+            color: '#999'
+          }
+        }
+      },
+      toolbox: {
+        feature: {
+          dataView: {show: true, readOnly: false},
+          magicType: {show: true, type: ['line', 'bar']},
+          restore: {show: true},
+          saveAsImage: {show: true}
+        }
+      },
+      legend: {
+        data:[
+          '一部计划金额(元)',
+          '二部计划金额(元)',
+          '一部实际金额(元)',
+          '二部实际金额(元)',
+          '一部完成率(%)',
+          '二部完成率(%)'
+        ]
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: month_arr,
+          axisPointer: {
+            type: 'shadow'
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          name: '金额',
+          min: 0,
+          max: 500000,
+          interval: 50000,
+          axisLabel: {
+            formatter: '{value} 元'
+          }
+        },
+        {
+          type: 'value',
+          name: '完成率',
+          min: 0,
+          max: 200,
+          interval: 20,
+          axisLabel: {
+            formatter: '{value} %'
+          }
+        }
+      ],
+      series: [
+        {
+          name:'一部计划金额(元)',
+          type:'bar',
+          data:data[0][0]
+        },
+        {
+          name:'二部计划金额(元)',
+          type:'bar',
+          data:data[1][0]
+        },
+        {
+          name:'一部实际金额(元)',
+          type:'bar',
+          data:data[0][1]
+        },
+        {
+          name:'二部实际金额(元)',
+          type:'bar',
+          data:data[1][1]
+        },
+        {
+          name:'一部完成率(%)',
+          type:'line',
+          yAxisIndex: 1,
+          data:data[0][2]
+        },
+        {
+          name:'二部完成率(%)',
+          type:'line',
+          yAxisIndex: 1,
+          data:data[1][2]
+        }
+      ]
+    };
+
+    myChart.setOption(option);
+
+  };
+  // End DOM method /renderBranchData/
   // ----------------------------- END DOM METHODS ---------------------------
 
   // -------------------------- BEGIN EVENT HANDLERS -------------------------
-  // example: onClick...
+  onClick = function () {
+    $('.chart-nav').unbind('click').click(function (event) {
+      var target = event.target || window.event.target;
+
+      switch ( $(target).attr('data-item') ) {
+        case '0' :
+          $('.chart_nav_item').removeClass('chart_nav_item_active');
+          $('.chart_nav_item:nth-child(1)').addClass('chart_nav_item_active');
+          // 获取部门图表数据
+          getData('getHistory', { suc : renderChart });
+          break;
+        case '1' :
+          $('.chart_nav_item').removeClass('chart_nav_item_active');
+          $('.chart_nav_item:nth-child(2)').addClass('chart_nav_item_active');
+          // 获取分部对比数据
+          getData('findDepartmentHistory', { suc : renderBranchData });
+          break;
+        default :
+          break;
+      }
+    });
+  };
   // --------------------------- END EVENT HANDLERS --------------------------
 
   // -------------------------- BEGIN PUBLIC METHODS -------------------------
@@ -163,10 +303,11 @@ var departmentChart = (function () {
     setJqueryMap();
 
     getData('getHistory', { suc : renderChart });
+    onClick();
   };
   // End public method /initModule/
 
   // return public methods
-  return { initModule : initModule }
+  return { initModule : initModule };
   // --------------------------- END PUBLIC METHODS --------------------------
 }());
